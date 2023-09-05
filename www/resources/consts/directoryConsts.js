2f0c7f7685368df7975f78b20f03b50f73ad7bb6
ahrimov <ahrimovanton@mail.ru>
2023-09-05 16:00:57 +0300

    init sgiomobmg

--- www/resources/consts/directoryConsts.js
+++ www/resources/consts/directoryConsts.js
@@ -1,8 +1,8 @@
 const main_directory = "file:///storage/self/primary/";
-const app_device_directory = "file:///storage/self/primary/Android/data/io.cordova.sgiomob/";
-const root_directory = "file:///storage/self/primary/Android/data/io.cordova.sgiomob/Project/";
-const media_directory = "file:///storage/self/primary/Android/media/io.cordova.sgiomob/";
+const app_device_directory = "file:///storage/self/primary/Android/data/io.cordova.sgiomobmg/";
+const root_directory = "file:///storage/self/primary/Android/data/io.cordova.sgiomobmg/Project/";
+const media_directory = "file:///storage/self/primary/Android/media/io.cordova.sgiomobmg/";
 const common_media_directory = "file:///storage/self/primary/Android/media/";
-const app_directory_name = "io.cordova.sgiomob";
+const app_directory_name = "io.cordova.sgiomobmg";
 
 let rosreestr_url = '';
